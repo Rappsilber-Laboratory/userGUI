6b514b1948a024fdd70f7f86be0788ee37774c45
colin-combe <colin.combe@ed.ac.uk>
2021-06-22 12:53:17 +0100

    fix?

--- userRegInnerDiv.php
+++ userRegInnerDiv.php
@@ -1,23 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-     <head>
-        <title>Create New Xi User Account</title>
-        <meta http-equiv="content-type" content="text/html; charset=UTF-8">
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-
-        <link rel="stylesheet" href="vendor/css/example.wink.css" />
-        <link rel="stylesheet" href="vendor/css/common.css" />
-        <link rel="stylesheet" href="userGUI/css/login.css" />
-
-        <script src="vendor/js/jquery-3.4.1.js"></script>
-        <script src="vendor/js/hideShowPassword.js"></script>
-        <script src="vendor/js/spin.js"></script>
-         <script src="userGUI/js/forms.js"></script>
-     </head>
-
-     <body>
-
         <header>
             <span class="topRight"><button id="helpButton"></button></span>
             <h1>Create New Xi User Account</h1>
